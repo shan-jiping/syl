765df8ec10ba79eb64b68f1a35cbe5e49d2de302
sjp <jiping505@163.com>
2017-06-22 18:09:54 +0800

    add search and RSS

--- my_blog/article/admin.py
+++ my_blog/article/admin.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 from article.models import Article
+
 # Register your models here.
+
 admin.site.register(Article)
