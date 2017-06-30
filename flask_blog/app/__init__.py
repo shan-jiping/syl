1e0694d0541a47230998920d702398875633571f
sjp <jiping505@163.com>
2017-06-30 15:55:42 +0800

    add flask blog

---
+++ flask_blog/app/__init__.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+
+app=Flask(__name__)
+app.config.from_object('config')
+db=SQLAlchemy(app)
+lm=LoginManager()
+lm.setup_app(app)
+
+
+
+from app import views,models
